c446899037cd5207435ade37afb15ec06f5c5496
Mainendra <1897340+mainendra@users.noreply.github.com>
2022-07-13 10:14:46 -0400

    Added lua config

---
+++ private_dot_config/nvim/lua/config.lua
@@ -0,0 +1,140 @@
+-------------------- HELPERS ------------------------------
+
+local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
+local g = vim.g      -- a table to access global variables
+
+local function map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-------------------- CONFIG -------------------------------
+g['mapleader'] = ' ' -- leader key
+
+-- let g:node_host_prog = '/usr/local/bin/neovim-node-host'
+g['node_host_prog'] = vim.call('system', 'volta which neovim-node-host | tr -d "\n"')
+
+g.coq_settings = {
+    auto_start = 'shut-up',
+}
+
+g['gitblame_date_format'] = '%r' -- relative date
+g['gitblame_enabled'] = 0 -- default disabled
+
+g['floaterm_gitcommit'] = 'floaterm'
+g['floaterm_autoinsert'] = 1
+g['floaterm_width'] = 0.9
+g['floaterm_height'] = 0.9
+g['floaterm_wintitle'] = 0
+g['floaterm_autoclose'] = 2
+
+g['startify_lists'] = {{type = 'bookmarks', header = {'Bookmarks'}}}
+g['startify_bookmarks'] = {
+    { i = '~/.config/nvim/init.lua' },
+    { p = '~/.config/nvim/lua/plugins.lua' },
+    { c = '~/.config/nvim/lua/config.lua' },
+    { o = '~/.config/nvim/lua/options.lua' },
+    { z = '~/.zshrc' }, { g = '~/.gitconfig' },
+    { b = '~/Library/Preferences/org.dystroy.broot/conf.toml' },
+    { t = '~/.tmux.conf'},
+    { l = '~/Library/Application\\ Support/jesseduffield/lazygit/config.yml' }
+}
+
+cmd [[au TextYankPost * silent! lua vim.highlight.on_yank {on_visual=false, timeout=200}]]
+
+
+-------------------- MAPPINGS -------------------------------
+
+map('n', '<Space>sv', ':source $MYVIMRC<CR>')
+
+map('n', '<Space>', '<Nop>', { noremap = true, silent = true })
+
+-- <Tab> to navigate the completion menu
+map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', {expr = true})
+map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
+
+map('n', '\\', '<cmd>noh<CR>')    -- Clear highlights
+
+map('n', '<Leader>j', ':j<CR>')
+map('n', '<Leader>J', ':j!<CR>')
+
+map('n', '<Leader>w', ':w<CR>')
+
+-- keep visual selection after indenting
+map('v', '>', '>gv');
+map('v', '<', '<gv');
+
+-- copy file path
+map('n', '<Leader>cp', ':let @*=expand("%")<CR>')
+
+-- terminal
+map('n', '<Leader>lg', ':FloatermNew lazygit<CR>');
+map('n', '<Leader>tg', ':FloatermNew tig %<CR>');
+
+-- Telescope
+map('n', '<Leader>fl', '<cmd>Telescope current_buffer_fuzzy_find theme=get_ivy layout_config={height=0.5}<CR>')
+map('n', '<Leader>ff', '<cmd>Telescope find_files theme=get_ivy layout_config={height=0.5}<CR>')
+map('n', '<Leader>fg', '<cmd>Telescope live_grep theme=get_ivy layout_config={height=0.5}<CR>')
+map('n', '<Leader>fb', '<cmd>Telescope buffers theme=get_ivy layout_config={height=0.5}<CR>')
+map('n', '<Leader>fh', '<cmd>Telescope help_tags theme=get_ivy layout_config={height=0.5}<CR>')
+map('n', '<Leader>fv', '<cmd>Telescope git_files theme=get_ivy layout_config={height=0.5}<CR>')
+map('n', '<Leader>fp', '<cmd>Telescope planets theme=get_ivy layout_config={height=0.5}<CR>')
+map('n', '<Leader>fk', '<cmd>Telescope keymaps theme=get_ivy layout_config={height=0.5}<CR>')
+
+-- ferret mappings
+map('n', '<Leader>x', ':Ack ')
+map('n', '<Leader>y', ':Lack ')
+map('n', '<Leader>ur', ':Ack <C-R><C-W><CR>')
+
+map('n', '<S-u>', '<C-u>')
+map('n', '<S-d>', '<C-d>')
+
+map('n', 'qo', ':only<CR>')
+
+map('', '<C-S-Left>', ':vertical resize -5<CR>')
+map('', '<C-S-Right>', ':vertical resize +5<CR>')
+map('', '<C-S-Up>', ':resize +5<CR>')
+map('', '<C-S-Down>', ':resize -5<CR>')
+
+
+map('n', '<Leader>e', ':NvimTreeFindFileToggle<CR>')
+
+-- switch window using hjkl
+map('n', '<S-h>', '<C-w>h')
+map('n', '<S-j>', '<C-w>j')
+map('n', '<S-k>', '<C-w>k')
+map('n', '<S-l>', '<C-w>l')
+
+-- move selection using jk
+map('v', '<S-j>', ':m\'>+<CR>gv=gv')
+map('v', '<S-k>', ':m-2<CR>gv=gv')
+
+-- disable recording macros
+map('n', 'q', '<Nop>')
+map('n', 'Q', '<Nop>')
+
+map('n', 'qq', ':q<CR>')
+map('n', 'QQ', ':q!<CR>')
+
+-- hop
+map('n', '<Leader>hc', ':HopChar1<cr>')
+map('n', '<Leader>hw', ':HopWord<cr>')
+map('n', '<Leader>hl', ':HopLine<cr>')
+map('n', '<Leader>hp', ':HopPattern<cr>')
+
+-- lsp config
+map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+map('n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+map('n', 'rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+
+-- startify
+map('n', '<Leader>S', ':Startify<CR>')
+
+map('n', '<Leader>gb', ':GitBlameToggle<CR>')
+
---
+++ private_dot_config/nvim/lua/plugins.lua
@@ -0,0 +1,230 @@
+-------------------- HELPERS ------------------------------
+
+local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+-------------------- PLUGINS ------------------------------
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+  execute 'packadd packer.nvim'
+end
+
+-- Only required if you have packer in your `opt` pack
+cmd [[packadd packer.nvim]]
+
+require('packer').startup(function()
+
+-- Packer can manage itself as an optional plugin
+use {'wbthomason/packer.nvim', opt = true}
+
+use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+    config = function()
+        require'nvim-treesitter.configs'.setup {
+            -- ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+            highlight = {
+                enable = true,              -- false will disable the whole extension
+                disable = {'lua'},          -- list of language that will be disabled
+            },
+            autotag = {
+                enable = true,
+            },
+        }
+    end
+}
+use {
+    "williamboman/nvim-lsp-installer",
+    "neovim/nvim-lspconfig",
+    config = function()
+        require("nvim-lsp-installer").setup {
+            automatic_installation = true
+        }
+    end,
+}
+local lsp_installer = require("nvim-lsp-installer")
+local lspconfig = require("lspconfig")
+lsp_installer.on_server_ready(function (server)
+    if server.name == 'tsserver' then
+        server:setup {root_dir = lspconfig.util.root_pattern("package.json")}
+    elseif server.name == 'denols' then
+        server:setup {root_dir = lspconfig.util.root_pattern("deno.json"),}
+    else
+        server:setup {}
+    end
+end)
+
+use {
+  'ojroques/nvim-lspfuzzy',
+  requires = {
+    {'junegunn/fzf'},
+    {'junegunn/fzf.vim'},  -- to enable preview (optional)
+  },
+  config = function()
+      require('lspfuzzy').setup {
+          methods = 'all',         -- either 'all' or a list of LSP methods (see below)
+          fzf_preview = {          -- arguments to the FZF '--preview-window' option
+          'right:+{2}-/2'          -- preview on the right and centered on entry
+          },
+          fzf_modifier = ':~:.',   -- format FZF entries, see |filename-modifiers|
+          fzf_trim = true,         -- trim FZF entries
+      }
+  end
+}
+
+-- improve default vim ui. e.g. code actions
+use { 'stevearc/dressing.nvim' }
+
+-- Auto completion
+use {
+    'ms-jpq/coq_nvim',
+    branch = 'coq',
+}
+use { 'ms-jpq/coq.artifacts', branch = 'artifacts' }
+use { 'ms-jpq/coq.thirdparty', branch = '3p' }
+
+-- vim registers
+use 'gennaro-tedesco/nvim-peekup'
+
+-- fuzzy finder
+use {
+  'nvim-telescope/telescope.nvim',
+  requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+}
+
+use 'andymass/vim-matchup'
+
+use {
+  'kyazdani42/nvim-tree.lua',
+  requires = {
+    'kyazdani42/nvim-web-devicons', -- optional, for file icons
+  },
+  config = function()
+      require("nvim-tree").setup()
+  end
+}
+
+use {
+  'lewis6991/gitsigns.nvim',
+  requires = {
+    'nvim-lua/plenary.nvim'
+  },
+  config = function()
+    require('gitsigns').setup()
+  end
+}
+
+use {
+    'f-person/git-blame.nvim',
+}
+
+-- Interactive interface for json files
+use 'gennaro-tedesco/nvim-jqx'
+
+-- terminal
+use {
+    'voldikss/vim-floaterm',
+}
+
+-- Commenting
+use {
+    'b3nj5m1n/kommentary',
+    config = function()
+        require('kommentary.config').use_extended_mappings()
+    end
+}
+
+-- easy motion
+use {
+    'phaazon/hop.nvim',
+    branch='v1',
+    config = function()
+        require('hop').setup{keys = 'etovxqpdygfblzhckisuran'}
+    end
+}
+
+use 'kevinhwang91/nvim-hlslens'
+
+use {
+    'echasnovski/mini.nvim',
+    branch = 'stable',
+    config = function()
+        require('mini.surround').setup()
+    end
+}
+
+-- quick fix fzf
+use {
+    'kevinhwang91/nvim-bqf',
+    config = function()
+        require('bqf').setup({
+            auto_enable = true,
+            preview = {
+                win_height = 12,
+                win_vheight = 12,
+                delay_syntax = 80,
+                border_chars = {'┃', '┃', '━', '━', '┏', '┓', '┗', '┛', '█'}
+            },
+            func_map = {
+                vsplit = '',
+                ptogglemode = 'z,',
+                stoggleup = ''
+            },
+            filter = {
+                fzf = {
+                    extra_opts = {'--bind', 'ctrl-o:toggle-all', '--prompt', '> '}
+                }
+            }
+        })
+    end
+}
+
+-- Ack search (vim script)
+use 'wincent/ferret'
+
+use {
+    'windwp/nvim-autopairs',
+    config = function()
+       require('nvim-autopairs').setup()
+    end
+}
+
+use {
+    'ellisonleao/gruvbox.nvim',
+    config = function()
+        require("gruvbox").setup({
+            contrast = "hard",
+        })
+        vim.cmd[[colorscheme gruvbox]]
+    end
+}
+
+use {
+    'mhinz/vim-startify',
+}
+use 'dstein64/vim-startuptime' -- startup time
+use 'markonm/traces.vim' -- search and replace
+use {
+    'hoob3rt/lualine.nvim',
+    requires = {'kyazdani42/nvim-web-devicons', opt = true}
+}
+
+-- jk to escape
+use {
+  "max397574/better-escape.nvim",
+  config = function()
+    require("better_escape").setup()
+  end
+}
+
+-- create missing directory on save
+use {
+  "jghauser/mkdir.nvim",
+}
+
+end)
+
