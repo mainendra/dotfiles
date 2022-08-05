5e55a96f9ad01280d1dd69e153e16d13480e5a13
Mainendra <1897340+mainendra@users.noreply.github.com>
2022-08-05 11:15:47 -0400

    Updated tresitter config

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -26,7 +26,7 @@ use {
     run = ':TSUpdate',
     config = function()
         require'nvim-treesitter.configs'.setup {
-            ensure_installed = 'all',
+            -- ensure_installed = 'all',
             auto_install = true,
             highlight = {
                 enable = true,
