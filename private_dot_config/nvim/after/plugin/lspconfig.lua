961c33791910db664845df721b54245036074620
Mainendra <1897340+mainendra@users.noreply.github.com>
2022-12-27 09:56:45 -0500

    Reverted K keymap back to default

--- private_dot_config/nvim/after/plugin/lspconfig.lua
+++ private_dot_config/nvim/after/plugin/lspconfig.lua
@@ -26,7 +26,7 @@ local on_attach = function(_, bufnr)
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts) -- using ufo
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
   vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
   vim.keymap.set('n', '<Leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
