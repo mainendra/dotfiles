21eb12e3c8e57c94234b2721d89c5cb38ef1fa92
Mainendra <1897340+mainendra@users.noreply.github.com>
2023-09-04 10:35:32 -0400

    Cleanup

--- private_dot_config/nvim/lua/lsp.lua
+++ private_dot_config/nvim/lua/lsp.lua
@@ -1,6 +1,6 @@
-local status_ok, mason, mason_lspconfig, lspconfig
+local status_ok, mason_lspconfig, lspconfig
 
-status_ok, mason = pcall(require, 'mason')
+status_ok = pcall(require, 'mason')
 if not status_ok then
   return
 end
@@ -39,18 +39,10 @@ local on_attach = function(client, bufnr)
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', '<Leader>do', vim.diagnostic.open_float , bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
   vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<Leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<Leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<Leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<Leader>D', vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
