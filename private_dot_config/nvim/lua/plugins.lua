8eb54bbd895eb2d80d012b6222be6dc769a3bfea
Mainendra <1897340+mainendra@users.noreply.github.com>
2023-11-17 10:31:23 -0500

    GitBlame workaround

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -167,6 +167,7 @@ require('lazy').setup({
         config = function()
             vim.g['gitblame_date_format'] = '%r' -- relative date
             vim.g['gitblame_enabled'] = 0        -- default disabled
+            vim.cmd('GitBlameToggle')            -- workaround
         end
     },
     {
