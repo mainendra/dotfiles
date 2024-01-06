7b20aa089e9346bc488e03e628405b274ed654b2
Mainendra <1897340+mainendra@users.noreply.github.com>
2024-01-06 15:14:10 -0500

    Used mini.pick instead of telescope

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -53,6 +53,15 @@ require('lazy').setup({
                     end
                 end
             },
+            -- picker
+            { '<Leader>fl', '<cmd>Pick buf_lines<CR>' },
+            { '<Leader>ff', '<cmd>Pick files<CR>' },
+            { '<Leader>fg', '<cmd>Pick grep_live<CR>' },
+            { '<Leader>fb', '<cmd>Pick buffers<CR>' },
+            { '<Leader>fh', '<cmd>Pick help<CR>' },
+            { '<Leader>fv', '<cmd>Pick git_files<CR>' },
+            { '<Leader>fk', '<cmd>Pick keymaps<CR>' },
+            { '<Leader>fc', '<cmd>lua MiniPick.builtin.files(nil, { source={ cwd="~/.config" } })<CR>' },
         },
         config = function()
             require('mini.ai').setup()
@@ -176,38 +185,6 @@ require('lazy').setup({
     {
         'tpope/vim-sleuth',
         event = { 'BufRead' }
-    },
-
-    -- Fuzzy Finder (files, lsp, etc)
-    {
-        'nvim-telescope/telescope.nvim',
-        branch = '0.1.x',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-        },
-        keys = {
-            { '<Leader>ft', '<cmd>Telescope builtin theme=get_ivy layout_config={height=0.5}<CR>' },
-            { '<Leader>fl', '<cmd>Telescope current_buffer_fuzzy_find theme=get_ivy layout_config={height=0.5}<CR>' },
-            { '<Leader>ff', '<cmd>Telescope find_files theme=get_ivy layout_config={height=0.5}<CR>' },
-            { '<Leader>fg', '<cmd>Telescope live_grep theme=get_ivy layout_config={height=0.5}<CR>' },
-            { '<Leader>fb', '<cmd>Telescope buffers theme=get_ivy layout_config={height=0.5}<CR>' },
-            { '<Leader>fh', '<cmd>Telescope help_tags theme=get_ivy layout_config={height=0.5}<CR>' },
-            { '<Leader>fv', '<cmd>Telescope git_files theme=get_ivy layout_config={height=0.5}<CR>' },
-            { '<Leader>fp', '<cmd>Telescope planets theme=get_ivy layout_config={height=0.5}<CR>' },
-            { '<Leader>fk', '<cmd>Telescope keymaps theme=get_ivy layout_config={height=0.5}<CR>' },
-            { '<Leader>fc', '<cmd>lua require("telescope.builtin").fd(require("telescope.themes").get_ivy({ prompt_title="Find config files", cwd="~/.config", hidden = true, layout_config={height=0.5} }))<CR>' },
-        },
-        cmd = 'Telescope',
-        opts = {
-            defaults = {
-                mappings = {
-                    i = {
-                        ['<C-u>'] = false,
-                        ['<C-d>'] = false,
-                    },
-                },
-            },
-        }
     },
 
     -- ui select and input
