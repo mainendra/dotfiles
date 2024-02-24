104093ade1033e8fc57e2319b0ca5af9d8e1d5f3
Mainendra <1897340+mainendra@users.noreply.github.com>
2024-02-24 11:06:41 -0500

    removed deprecated null_ls plugins

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -120,14 +120,10 @@ require('lazy').setup({
                 log_level = 'off',
                 sources = {
                     null_ls.builtins.formatting.stylua,
-                    null_ls.builtins.diagnostics.deno_lint,
                     null_ls.builtins.code_actions.gitsigns,
-                    null_ls.builtins.code_actions.ltrs,
                     null_ls.builtins.code_actions.refactoring,
                     null_ls.builtins.completion.spell,
-                    null_ls.builtins.formatting.deno_fmt,
                     null_ls.builtins.formatting.rustywind,
-                    null_ls.builtins.formatting.rustfmt,
                 },
             })
         end,
