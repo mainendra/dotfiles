97eb43163e85048ed9306342b12fdecb964e3890
Mainendra <1897340+mainendra@users.noreply.github.com>
2024-06-22 13:46:40 -0400

    Added wezterm config

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,24 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'GruvboxDarkHard'
+config.font = wezterm.font 'JetBrainsMono Nerd Font'
+config.font_size = 14.0
+config.window_decorations = 'RESIZE'
+config.enable_tab_bar = false
+config.audible_bell = 'Disabled'
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+-- and finally, return the configuration to wezterm
+return config
