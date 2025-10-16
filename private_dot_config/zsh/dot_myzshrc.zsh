1399dd6c0d29ceb2ff431eef54233cf7793629b6
Mainendra <1897340+mainendra@users.noreply.github.com>
2025-10-16 12:29:12 -0400

    fixed typo

--- private_dot_config/zsh/dot_myzshrc.zsh
+++ private_dot_config/zsh/dot_myzshrc.zsh
@@ -8,7 +8,7 @@ alias arm="$env /usr/bin/arch -arm64 /bin/zsh ---login"
 alias gtb="git commit --allow-empty -m 'Trigger build'"
 
 # alias ip="ifconfig | ack \"inet ([0-9]+.[0-9]+.[0-9]+.[0-9]+)\" --output \"$1\""
-if [ "$(command -v ipconfig)" ]; then
+if [ "$(command -v ifconfig)" ]; then
     alias ip="ifconfig | ack 'inet ([0-9]+.[0-9]+.[0-9]+.[0-9]+)' --output '\$1'"
 fi
 if [ "$(command -v ip)" ]; then
