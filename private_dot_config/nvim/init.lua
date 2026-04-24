0c753e5a4cae15ba4524507b411f4f840e855a23
Mainendra <1897340+mainendra@users.noreply.github.com>
2026-04-24 11:46:50 -0600

    Used brew packages for neovim

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,3 +1,7 @@
+if vim.fn.isdirectory("/opt/homebrew/bin") == 1 then
+  vim.env.PATH = "/opt/homebrew/bin:/opt/homebrew/sbin:" .. vim.env.PATH
+end
+
 require('options');
 require('config');
 require('plugins');
