0472b2dfdbbc48376ae8a1874582139f0a8780b7
Mainendra <1897340+mainendra@users.noreply.github.com>
2026-05-16 07:33:07 -0600

    zsh config cleanup

--- private_dot_config/zsh/dot_myzshrc.zsh
+++ private_dot_config/zsh/dot_myzshrc.zsh
@@ -1,148 +1,101 @@
-################ Alias ################
+# ─── Navigation ────────────────────────────────────────────────────────────────
 
-# Switch terminal architecture type:
-alias x86="$env /usr/bin/arch -x86_64 /bin/zsh ---login"
-alias arm="$env /usr/bin/arch -arm64 /bin/zsh ---login"
+alias cd..='cd ..'
+alias ..='cd ..'
+alias ...='cd ../../../'
+alias ....='cd ../../../../'
+alias .5='cd ../../../../..'
 
-# mac
+# ─── Git ──────────────────────────────────────────────────────────────────────
+
 alias gtb="git commit --allow-empty -m 'Trigger build'"
 
-# alias ip="ifconfig | ack \"inet ([0-9]+.[0-9]+.[0-9]+.[0-9]+)\" --output \"$1\""
+# ─── Tmux ─────────────────────────────────────────────────────────────────────
+
+alias ta="tmux a"
+alias tk="tmux kill-server"
+alias tc="tmux capture-pane -pS - -E - | nvim -"
+
+# ─── Tools ────────────────────────────────────────────────────────────────────
+
+alias lg="lazygit"
+alias ld="lazydocker"
+alias br="broot"
+alias pn="pnpm"
+alias nfzf='nvim $(fzf)'
+alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
+
+# ─── Better defaults ─────────────────────────────────────────────────────────
+
+if command -v xh > /dev/null 2>&1; then
+    alias http='xh'
+fi
+alias cat='bat --style=plain'
+alias l='eza --color=auto --icons -l'
+alias la='l -la'
+alias ll='l -lah'
+alias bc='bc -l'
+alias ping='ping -c 5'
+alias fastping='ping -c 100 -s 2'
+
+# ─── Networking ───────────────────────────────────────────────────────────────
+
 if [ "$(command -v ifconfig)" ]; then
     alias ip="ifconfig | ack 'inet ([0-9]+.[0-9]+.[0-9]+.[0-9]+)' --output '\$1'"
 elif [ "$(command -v ip)" ]; then
     alias ip="ip address | ack 'inet ([0-9]+.[0-9]+.[0-9]+.[0-9]+)' --output '\$1'"
 fi
 alias gip="http -4 https://ipecho.io/plain -p b"
-
-alias ta="tmux a"
-alias tk="tmux kill-server"
-alias tc="tmux capture-pane -pS - -E - | nvim -"
-alias lg="lazygit"
-alias ld="lazydocker"
-alias gu="gitu"
-alias br="broot"
-
-## get rid of command not found ##
-alias cd..='cd ..'
-
-## a quick way to get out of current directory ##
-alias ..='cd ..'
-alias ...='cd ../../../'
-alias ....='cd ../../../../'
-alias .....='cd ../../../../'
-alias .4='cd ../../../../'
-alias .5='cd ../../../../..'
-
-# calculator with bash support
-alias bc='bc -l'
-
-# handy short cuts #
-alias h='history'
-alias j='jobs -l'
-
-# date time
-alias path='echo -e ${PATH//:/\\n}'
-alias now='date +"%T"'
-alias nowtime=now
-alias nowdate='date +"%d-%m-%Y"'
-
-# Stop after sending count ECHO_REQUEST packets #
-alias ping='ping -c 5'
-# Do not wait interval 1 second, go fast #
-alias fastping='ping -c 100 -s 2'
-
-# get web server headers #
 alias header='curl -I'
-
-# find out if remote server supports gzip / mod_deflate or not #
 alias headerc='curl -I --compress'
 
-# default utils
-if command -v batcat > /dev/null 2>&1; then # on pi
-    alias bat='batcat'
-fi
-alias cat='bat --style=plain'
-alias l='eza --color=auto --icons -l'
-alias la='l -la'
-alias ll='l -lah'
+# ─── Info ─────────────────────────────────────────────────────────────────────
 
-# Chrome
-alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
-alias chrome-canary="/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary"
-# bookmark manager
-alias cb='chrome $(buku -p -f 10 | fzf)'
-# chrome with security disabled
-alias chrome-ds='open -na Google\ Chrome --args --user-data-dir=/tmp/temporary-chrome-profile-dir --disable-web-security --disable-site-isolation-trials'
+alias h='history'
+alias j='jobs -l'
+alias path='echo -e ${PATH//:/\\n}'
+alias now='date +"%T"'
+alias nowdate='date +"%d-%m-%Y"'
+
+# ─── Python ───────────────────────────────────────────────────────────────────
 
 alias venv='python -m venv ./venv && echo "layout_python3" > .envrc && direnv allow'
 alias pipreq='pip freeze > requirements.txt'
 
-# VLC video player (mac)
-alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
+# ─── Chrome ───────────────────────────────────────────────────────────────────
 
-# pnpm
-alias pn=pnpm
+alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+alias chrome-canary="/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary"
+alias chrome-ds='open -na Google\ Chrome --args --user-data-dir=/tmp/temporary-chrome-profile-dir --disable-web-security --disable-site-isolation-trials'
 
-# forgit
-alias gf='git forgit'
+# ─── Proxy ────────────────────────────────────────────────────────────────────
 
-# fzf nvim
-alias nfzf='nvim $(fzf)'
-
-# git branch spell check
-alias bspell='git diff origin/develop...$(git branch -r --format="%(refname:short)" | fzf) | grep '^+' | grep -v '^+++' | sed 's/^+//' | cspell stdin --unique --config ~/.config/cspell/cspell.json'
-
-# pi
-if command -v xh > /dev/null 2>&1; then # on pi
-    alias http='xh'
-fi
-
-# aichat
-alias a#='aichat'
-
-################ Utils functions ################
-
-# proxy
 function enable-proxy() {
     sudo networksetup -setsecurewebproxy Wi-Fi 127.0.0.1 $1
     sudo networksetup -setwebproxy Wi-Fi 127.0.0.1 $1
 }
 alias disable-proxy="sudo networksetup -setsecurewebproxystate Wi-Fi off && sudo networksetup -setwebproxystate Wi-Fi off"
 
-# find-in-file - usage: fif <SEARCH_TERM>
+# ─── Utility functions ────────────────────────────────────────────────────────
+
+# find-in-file: usage `fif <SEARCH_TERM>`
 fif() {
-  if [ ! "$#" -gt 0 ]; then
-    echo "Need a string to search for!";
-    return 1;
-  fi
-  rg --files-with-matches --no-messages "$1" | fzf $FZF_PREVIEW_WINDOW --preview "rg --ignore-case --pretty --context 10 '$1' {}"
+    if [ ! "$#" -gt 0 ]; then
+        echo "Need a string to search for!"
+        return 1
+    fi
+    rg --files-with-matches --no-messages "$1" | fzf $FZF_PREVIEW_WINDOW --preview "rg --ignore-case --pretty --context 10 '$1' {}"
 }
 
-# Utils
-cheat() {
-    http -b cheat.sh/"$1"
-}
-wttr() {
-    http -b wttr.in/"$1"
-}
-rate() {
-    http -b rate.sx/"$1"
-}
+# query cheat.sh, wttr.in, rate.sx
+cheat() { http -b cheat.sh/"$1"; }
+wttr()  { http -b wttr.in/"$1"; }
+rate()  { http -b rate.sx/"$1"; }
 
-# resend key for pop
+# send email via resend/pop
 email() {
     if [[ -z $RESEND_API_KEY ]]; then
         export RESEND_API_KEY=$(pass RESEND_API_KEY)
     fi
     pop "$@"
 }
-
-# tsm - Terminal session manager
-tsm_palette() {
-  zle -I
-  tsm p
-  zle reset-prompt
-}
-zle -N tsm_palette
-bindkey '^g' tsm_palette
